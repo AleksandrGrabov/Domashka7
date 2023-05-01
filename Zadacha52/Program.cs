748fcf7f59851c08a732bfd1c1fd1618151977fc
AleksandrGrabov <grabov-it@yandex.by>
2023-05-01 23:11:06 +0300

    Решили задачу 52

---
+++ Zadacha52/Program.cs
@@ -0,0 +1,14 @@
+﻿int[,] array = { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
+        int rows = array.GetLength(0);
+        int columns = array.GetLength(1);
+
+        for (int j = 0; j < columns; j++)
+        {
+            int sum = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                sum += array[i, j];
+            }
+            double average = (double)sum / rows;
+            Console.WriteLine("Среднее арифметическое элементов столбца " + (j + 1) + ": " + Math.Round(average, 2));
+        }
