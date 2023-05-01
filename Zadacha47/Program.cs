4a990152402547ac975439df6a688ddb7096c188
AleksandrGrabov <grabov-it@yandex.by>
2023-05-01 22:35:25 +0300

    Решили задачу 47

---
+++ Zadacha47/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите количество строк m:");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите количество столбцов n:");
+        int n = int.Parse(Console.ReadLine());
+
+        double[,] array = new double[m, n];
+
+        Random random = new Random(); 
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                array[i, j] = Math.Round(random.NextDouble() * 20 - 10, 2); 
+            }
+        }
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(array[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+    
