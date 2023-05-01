edf79cc55a311f86061f78b0521609543f8a4d23
AleksandrGrabov <grabov-it@yandex.by>
2023-05-01 22:55:21 +0300

    Решили задачу 50

---
+++ Zadacha50/Program.cs
@@ -0,0 +1,20 @@
+﻿int[,] array = {
+            {1, 4, 7, 2},
+            {5, 9, 2, 3},
+            {8, 4, 2, 4}
+        }; 
+
+        Console.WriteLine("Введите номер строки:");
+        int row = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите номер столбца:");
+        int column = int.Parse(Console.ReadLine());
+
+        if (row >= 0 && row < array.GetLength(0) && column >= 0 && column < array.GetLength(1))
+        {
+            Console.WriteLine($"Значение элемента: {array[row, column]}");
+        }
+        else
+        {
+            Console.WriteLine("такого числа в массиве нет");
+        }
